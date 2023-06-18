4906374f3db770d007840bc986a3f1800bb8feb6
Chuong Le <redocecin@gmail.com>
2023-06-18 18:16:54 +0700

    update release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-rm -f /tmp/c3-cloudfront-clear-cache.zip
-cd ..
-zip -r /tmp/c3-cloudfront-clear-cache.zip ./c3-cloudfront-clear-cache -x "c3-cloudfront-clear-cache/.git/*" "c3-cloudfront-clear-cache/.github/*" "c3-cloudfront-clear-cache/node_modules/*" "c3-cloudfront-clear-cache/vendor/*" "c3-cloudfront-clear-cache/.distignore" "c3-cloudfront-clear-cache/.editorconfig" "c3-cloudfront-clear-cache/.phpcs.xml.dist" "c3-cloudfront-clear-cache/.wp-env.json" "c3-cloudfront-clear-cache/phpunix.xml" "c3-cloudfront-clear-cache/tests/*" "c3-cloudfront-clear-cache/renovate.json"
+# Build
+rm -rdf vendor
+php composer.phar install --no-dev
+
+# Bundle
+rm -f /tmp/c3-cloudfront-clear-cache-custom.zip
+rm -rdf /tmp/c3-cloudfront-clear-cache-custom
+mkdir /tmp/c3-cloudfront-clear-cache-custom
+cp -r classes templates vendor c3-cloudfront-clear-cache.php composer.json loader.php package.json readme.txt uninstall.php /tmp/c3-cloudfront-clear-cache-custom
+cd /tmp
+zip -r /tmp/c3-cloudfront-clear-cache-custom.zip ./c3-cloudfront-clear-cache-custom
